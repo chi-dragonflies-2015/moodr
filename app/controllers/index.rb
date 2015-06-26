b06631db1440d8702e48b6c1472656580b24259f
Nick Siefken <nicks720@gmail.com>
2015-06-26 11:38:05 -0500

    ran a test and it worked

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,12 +1,19 @@
 get '/' do
+  client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "vaENW1Csec23bA6077AHWLoAy"
+  config.consumer_secret     = "4x6FsXiiQG4nmp2b4eT6fgWqIKl9teMo1chOR8qqtmz5ILXAqi"
+  config.access_token        = "32813528-GUg4eTRcBxuisJWImOFr7qUCydjhdRvk3D6XHeoin"
+  config.access_token_secret = "lpglpvbfYLZUOeeyAizegdHriWFokuPjcn8BeaZwHxVsy"
+end
 
 #Chicago#
   @chicago_tweets = []
-  client.search(/./, lang: "en", geocode: "41.884251,-87.632446,300mi").take(10).each do |tweet|
+  client.search(/./, lang: "en", geocode: "41.884251,-87.632446,300mi").take(200).each do |tweet|
     @chicago_tweets << tweet.text
   end
   @chicago = Location.create(city: "Chicago", state: "Illinois")
-  @mood = Mood.new(location_id: @chicago.id)
+  @mood = Mood.new
+  @mood.location_id = @chicago.id
   @mood.score = @mood.mood_scorer(@chicago_tweets)
   @mood.happy_words = @mood.happy_counter
   @mood.sad_words = @mood.sad_counter
