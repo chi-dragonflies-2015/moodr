b15d5c4f7f6a3c1bde26d074afecf3f195669911
Bo Guthrie & Nick Siefken <pair+boguth.nsiefken@devbootcamp.com>
2015-06-26 13:49:51 -0500

    committing the balls

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,17 +1,16 @@
 get '/' do
 
 
-  @tweets = []
-  client.search(/./, lang: "en", geocode: "41.884251,-87.632446,300mi").take(10).each do |tweet|
-    @tweets << tweet.text
-  end
+  # @tweets = []
+  # client.search(/./, lang: "en", geocode: "41.884251,-87.632446,300mi").take(10).each do |tweet|
+  #   @tweets << tweet.text
+  # end
 
 
 
-  @tweets1 = []
-  client.search(/./, lang: "en", geocode: "37.777119,-122.419640,300mi").take(10).each do |tweet|
-    @tweets1 << tweet.text
-  end
+  # @tweets1 = []
+  #   @tweets1 << tweet.text
+  # end
   erb :index
 
   # @tweets2 = []
--- config/environment.rb
+++ config/environment.rb
@@ -21,6 +21,14 @@ require 'net/http'
 require 'erb'
 require 'httparty'
 
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "vaENW1Csec23bA6077AHWLoAy"
+  config.consumer_secret     = "4x6FsXiiQG4nmp2b4eT6fgWqIKl9teMo1chOR8qqtmz5ILXAqi"
+  config.access_token        = "32813528-GUg4eTRcBxuisJWImOFr7qUCydjhdRvk3D6XHeoin"
+  config.access_token_secret = "lpglpvbfYLZUOeeyAizegdHriWFokuPjcn8BeaZwHxVsy"
+end
+
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
