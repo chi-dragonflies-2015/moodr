e1fe99f3c8a2e768c9ea682a9cc94c55d387a17a
Nick Siefken <nicks720@gmail.com>
2015-06-26 10:42:48 -0500

    finish migrations

---
+++ db/migrate/20150626102400_create_tables.rb
@@ -0,0 +1,18 @@
+class CreateTables < ActiveRecord::Migration
+  def change
+    create_table :moods do |t|
+      t.references :location
+      t.integer :score
+      t.integer :happy_words
+      t.integer :sad_words
+
+      t.timestamps
+    end
+
+    create_table :locations do |t|
+      t.string :city
+      t.string :state
+
+      t.timestamps
+  end
+end
