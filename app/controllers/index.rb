790079309e4b20fa0416de14ab51731b0ae5219e
Nick Siefken <nicks720@gmail.com>
2015-06-26 10:21:34 -0500

    wrote method for mood score, plan to work on migrations next

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,22 +1,27 @@
 get '/' do
 
-
-  @tweets = []
+#Chicago#
+  @chicago_tweets = []
   client.search(/./, lang: "en", geocode: "41.884251,-87.632446,300mi").take(10).each do |tweet|
-    @tweets << tweet.text
+    @chicago_tweets << tweet.text
   end
+  @mood_score = mood_scorer(@chicago_tweets)
+  @mood = Mood.new(location_id: 1, score: @mood_score)
 
-
-
-  @tweets1 = []
-  client.search(/./, lang: "en", geocode: "37.777119,-122.419640,300mi").take(10).each do |tweet|
-    @tweets1 << tweet.text
-  end
-  erb :index
-
-  # @tweets2 = []
-  # client.search(/./, result_type: "recent", locations: "New York", lang: "en").take(200).each do |tweet|
-  #   @tweets2 << tweet.text
+#SF#
+  # @sf_tweets = []
+  # client.search(/./, lang: "en", geocode: "37.777119,-122.419640,300mi").take(10).each do |tweet|
+  #   @sf_tweets << tweet.text
   # end
-  # erb :index
+
+
+#New York#
+  # @ny_tweets = []
+  # client.search(/./, result_type: "recent", locations: "New York", lang: "en").take(200).each do |tweet|
+  #   @ny_tweets << tweet.text
+  # end
+
+
+
+  erb :index
 end
---
+++ app/models/mood.rb
@@ -0,0 +1,44 @@
+class Mood < ActiveRecord::Base
+  attr_accessor :happy_counter, :sad_counter
+
+  def initialize
+    @happy_counter = 0
+    @sad_counter = 0
+    @happy_words = ["happy", "grateful", "joyful", "smiling", "awesome", "fuck yeah!", "woo!", "yea!!", "yay", "great", "excellent", "kowabunga!", "Terrific!", "Wonderful", "terrific", "sappy", "optimistic", "adorable", "admired", "alive", "amazing", "blessed", "brilliant", "cheerful", "delictious", "elegant", "exciting", "fun", "impressive", "lucky"]
+    @sad_words = ["gloomy","tragic","depressive", "bleeding", "bummed out", "crummy", "dejected", "fed up", "glum", "grim", "low-spirited", "melancholy", "moody", "pessimistic", "sad", "unhappy", "death", "dead", "fear", "ruined", "lonely", "miserable", "anxious", "grief", "sorrow", "crying", "pathetic", "terrified", "scared","alone"]
+  end
+
+  # def mood_scorer(tweet)
+  #   self.happy_words.each do |word|
+  #     if tweet.include?(word)
+  #       happy_counter += 1
+  #     end
+  #   end
+
+  #   self.sad_words.each do |word|
+  #     if tweet.include?(word)
+  #       sad_counter += 1
+  #     end
+  #   end
+
+
+  # end
+
+
+  def mood_scorer(tweets_array)
+    tweets_array.each do |tweet|
+      @happy_words.each do |word|
+        if tweet.include?(word)
+          happy_counter += 1
+        end
+      end
+      @sad_words.each do |word|
+        if tweet.include?(word)
+          sad_counter += 1
+        end
+      end
+    end
+    happy_counter / sad_counter
+  end
+
+end
