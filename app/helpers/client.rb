f6f957ad650c579a431420972bec1b91807e565b
Bo Guthrie <bo.guthrie.mail@gmail.com>
2015-07-01 11:09:53 -0500

    Update client.rb

--- app/helpers/client.rb
+++ app/helpers/client.rb
@@ -1,12 +1,7 @@
 helpers do
 
 # def client
-#   Twitter::REST::Client.new do |config|
-#     config.consumer_key        = "vaENW1Csec23bA6077AHWLoAy"
-#     config.consumer_secret     = "4x6FsXiiQG4nmp2b4eT6fgWqIKl9teMo1chOR8qqtmz5ILXAqi"
-#     config.access_token        = "32813528-GUg4eTRcBxuisJWImOFr7qUCydjhdRvk3D6XHeoin"
-#     config.access_token_secret = "lpglpvbfYLZUOeeyAizegdHriWFokuPjcn8BeaZwHxVsy"
-#   end
+ Fill in twitter stuff here.
 # end
 
 end
